2cefc5cf27018f390fd748eaa15a5e0700aa3df0
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-11-11 19:43:07 +0000

    day 45 progress

--- main.py
+++ main.py
@@ -4,37 +4,40 @@ import requests
 response = requests.get("https://news.ycombinator.com/")
 yc_web_page = response.text
 soup = BeautifulSoup(yc_web_page, "html.parser")
-print(soup)
-
-def get_titles1():
-  post_titles = soup.find_all("td")
-  print(len(post_titles))
-  for i in range(len(post_titles)):
-    print(post_titles[i].find_all("a"))
 
 
-def get_titles2():
-  post_titles = soup.soup.find_all(name="td", class_="headtitle")
-  print(post_titles)
-
-def get_titles3():
+def get_titles():
   
-  post_titles = soup.find_all("a")
+  post_titles = soup.find_all(name="td", class_="title")
+  counter = 0
   for i in post_titles:
-    print(i.get("href"))
+    if counter < 1 :
+      counter += 1
+      print(i.getText(), end="")
+    else:
+      counter = 0
+      print(i.getText())
   # print(soup.select_one(selector="span a"))
 
-get_titles3()
+
+
+
+def get_article_link():
+  
+ soup.a.parent.name
+  # print(soup.select_one(selector="span"))
+
+get_titles()
+
+
+
+def custom_selector(tag):
+	# Return "span" tags with a class name of "target_span"
+	return tag.name == "span" and tag.has_attr("class") and "target_span" in tag.get("class")
+
+
 # for i in post_titles:
 #   print(f' record {i}')
-
-
-
-
-
-
-
-
 
 
 # import lxml
