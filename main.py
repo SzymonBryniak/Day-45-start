897514fdc4999d3422c2557d32918364479be58d
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-11-15 19:25:42 +0000

    Day 45 progress

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
-
+import threading
+import time
 response = requests.get("https://news.ycombinator.com/")
 yc_web_page = response.text
 soup = BeautifulSoup(yc_web_page, "html.parser")
 
+############################################################################# print 
 
 def get_titles_numbered():
+  time.sleep(0.3)
   post_titles = soup.find_all(name="td", class_="title")
   counter = 0
   for i in post_titles:
@@ -16,39 +19,48 @@ def get_titles_numbered():
     else:
       counter = 0
       print(i.getText())
-  # print(soup.select_one(selector="span a"))
-
+    
 
 def get_titles():
+  time.sleep(0.3)
   post_titles = soup.find_all(class_="titleline")
-  counter = 0
   for i in post_titles:
     print(i.getText())
-
+  
 
 def get_article_upvote():
+  time.sleep(0.3)
   post_titles = soup.find_all(name="span", class_="score")
   for i in post_titles:
     print(i.getText())
-    
-  # print(soup.select_one(selector="span"))
-
+  
 
 def get_href():
   post_titles = soup.find_all(class_="titleline")
   for i in post_titles:
     print(i.find('a').get('href'))
+  
+  
+get_titles_numbered()
+get_article_upvote()
+get_href()
+
+# thread1 = threading.Thread(target=get_titles_numbered)
+# thread2 = threading.Thread(target=get_article_upvote)
 
 
+# thread1.start()
+# thread2.start()
+
+
+# thread1.join()
+# thread2.join()
+
+
+
+############################################################################# concatenated 
 # link = soup.select('span').find('a')
 # print(link)
-
-
-
-
-
-
-
 # for i in post_titles:
 #   print(f' record {i}')
 
